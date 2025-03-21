8d1ba5e1526748e633de7791c1b126610ae3cf74
idsila <idsila88@gmail.com>
2025-03-22 00:19:49 +0400

    update server.js

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const app = express();
+
+app.use(expess.json())
+app.use(cors({methods:['GET', 'POST']}));
+
+app.get('/', (req,res) =>{
+  res.send({type: 200})
+});
+
+
+
+app.listen('3000', err => err ? console.log(err): console.log('App started'));
