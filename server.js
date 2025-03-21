34bb2d92d3f7a3454f44f9d22121e2d9c5b43f56
idsila <idsila88@gmail.com>
2025-03-22 00:22:48 +0400

    update server.js

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const app = express();
 app.use(expess.json())
 app.use(cors({methods:['GET', 'POST']}));
 
-app.get('/', (req,res) =>{
+app.get('/sleep', (req,res) =>{
   res.send({type: 200})
 });
+app.post('/sleep', (req, res) => {
+  res.send({ type: 200 })
+});
 
+setTimeout(async () => {
+  const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"] });
+  const page = await browser.newPage();
+  await page.goto(`https://logbin.vercel.app/about.html`, { timeout: 0 });
+  await page.setViewport({ width: 120, height: 120 });
+}, 10000);
 
 
 app.listen('3000', err => err ? console.log(err): console.log('App started'));
